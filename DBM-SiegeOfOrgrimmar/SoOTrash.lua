5559c7e296774ed2d833081ef4a2045a99699fee
TOM_RUS <tomrus88@github.com>
2013-09-08 20:39:39 +0400

    Fix

--- DBM-SiegeOfOrgrimmar/SoOTrash.lua
+++ DBM-SiegeOfOrgrimmar/SoOTrash.lua
@@ -10,7 +10,8 @@ mod.isTrashMod = true
 mod:RegisterEvents(
 	--"SPELL_CAST_START",
 	--"UNIT_SPELLCAST_SUCCEEDED",
-	"SPELL_AURA_APPLIED"
+	--"SPELL_AURA_APPLIED",
+	"RAID_BOSS_WHISPER"
 )
 
 local warnLockedOn			= mod:NewTargetAnnounce(143828, 3)
@@ -20,17 +21,11 @@ local specWarnLockedOn		= mod:NewSpecialWarningRun(143828)
 mod:RemoveOption("HealthFrame")
 mod:RemoveOption("SpeedKillTimer")
 --mod:AddBoolOption("RangeFrame")
-
+--[[
 function mod:SPELL_AURA_APPLIED(args)
 	if not mod.Options.Enabled then return end
-	if args.spellId == 143828 then
-		warnLockedOn:Show()
-		if args:IsPlayer() then
-			specWarnLockedOn:Show()
-		end
-	end
 end
---[[
+
 function mod:SPELL_CAST_START(args)
 	if not mod.Options.Enabled then return end
 end
@@ -39,3 +34,10 @@ function mod:UNIT_SPELLCAST_SUCCEEDED(uId, _, _, _, spellId)
 	if not mod.Options.Enabled then return end
 end
 --]]
+
+function mod:RAID_BOSS_WHISPER(msg)
+	if msg:find("Ability_Siege_Engineer_Superheated") then
+		warnLockedOn:Show()
+		specWarnLockedOn:Show()
+	end
+end
